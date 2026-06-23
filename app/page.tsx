13da182f72c04b729715dfadf2dcdee05fb3ff36
cmchey89 <cmchey89@gmail.com>
2026-06-23 19:51:28 +0800

    Redirect root to login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-// app/page.tsx
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  return (
-    <main className="zone-detail">
-      <h1>DigClear</h1>
-      <p>Underground utility clearance for working zones.</p>
-      <p>
-        <Link href="/login">Sign in</Link>
-      </p>
-    </main>
-  );
+  redirect('/login');
 }
